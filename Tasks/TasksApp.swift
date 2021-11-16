4efc4c4282ee8ab67c298fd8be1120d83dbdd9f2
jorgefperes <jorge-peres@live.com>
2021-11-16 09:30:47 -0300

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Jorge Peres on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
